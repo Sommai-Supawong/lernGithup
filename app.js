fff9084112fdd93599140506a57fc78285cfc694
Sommai-Supawong <Somai.050449@gmail.com>
2026-06-16 11:08:40 +0700

    add new branch

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function App() {
                 <h2>My Website</h2>
                 <nav>
                     <a href="#home">Home</a>
-                    <a href="#about">About</a>
+                    <a href="#about">Abouts</a>
                     <a href="#contact">Contact</a>
                 </nav>
             </header>
